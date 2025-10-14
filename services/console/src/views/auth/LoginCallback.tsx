28230293031e6b7f74a03c2bda72a2b7ea01a4f8
Jeroen Rinzema <jeroen@rinzema.dev>
2025-10-14 11:33:06 +0200

    fix: await session before redirect

--- services/console/src/views/auth/LoginCallback.tsx
+++ services/console/src/views/auth/LoginCallback.tsx
@@ -1,19 +1,25 @@
 import { useParams, useSearchParams } from 'react-router'
 import { useEffect } from 'react'
+import { useClerk } from '@clerk/clerk-react'
 import api from '../../api'
 
 import './Auth.css'
 
 export default function LoginCallback() {
+    const { session } = useClerk()
     const { driver } = useParams() as { driver: string }
     const [searchParams] = useSearchParams()
     const redirect = searchParams.get('r') ?? '/'
 
     const handleAuth = async () => {
         switch (driver) {
-        case 'cloud':
+        case 'cloud':{
+            if (!session) return
+
+            await session.getToken()
             await api.auth.cloudAuth(redirect)
             break
+        }
         }
 
         window.location.href = redirect
@@ -23,7 +29,7 @@ export default function LoginCallback() {
         handleAuth().catch((err) => {
             console.error('Authentication error', err)
         })
-    }, [driver, redirect])
+    }, [driver, redirect, session])
 
     // TODO: handle callback error
     return <></>
