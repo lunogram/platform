375e67b722af6c8d0f58d41d0504d6ba7d6bd023
Jeroen Rinzema <jeroen@rinzema.dev>
2025-10-21 00:21:48 +0200

    feat: include tailwindcss

--- services/console/vite.config.ts
+++ services/console/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-    // Load env file based on `mode` (e.g. .env.development or .env.production)
     const env = loadEnv(mode, process.cwd(), '')
 
     return {
         base: '/',
-        plugins: [react()],
+        plugins: [react(), tailwindcss()],
         server: {
             proxy: {
                 '/api': {
