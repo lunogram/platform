869dfe6f4f23d8a7fb937d9a0a9987bd8ca9e201
Jeroen Rinzema <jeroen@rinzema.dev>
2025-10-21 17:58:43 +0200

    chore: updated the alias @ paths

--- services/console/vite.config.ts
+++ services/console/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
@@ -35,7 +36,7 @@ export default defineConfig(({ mode }) => {
         },
         resolve: {
             alias: {
-                '@': '/src',
+                "@": path.resolve(__dirname, "./src"),
             }
         }
     }
