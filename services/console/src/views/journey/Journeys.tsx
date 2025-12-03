ce42d1a2dcd396d73b4217a86b8c2e047d1c5426
Jelger Dijkgraaf <jelger.dijkgraaf@wellsync.com>
2025-12-03 11:45:33 +0100

    fix: make 'create journey' button/form use shadcn

--- services/console/src/views/journey/Journeys.tsx
+++ services/console/src/views/journey/Journeys.tsx
@@ -2,7 +2,14 @@ import { useCallback, useContext, useState } from 'react'
 import { useNavigate } from 'react-router'
 import api from '../../api'
 import { Button } from '@/components/ui/button'
-import Modal from '../../ui/Modal'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
 import PageContent from '../../ui/PageContent'
 import { SearchTable, useSearchTableQueryState } from '../../ui/SearchTable'
 import { ArchiveIcon, DuplicateIcon, EditIcon, PlusIcon } from '../../components/icons'
@@ -45,10 +52,25 @@ export default function Journeys() {
         <PageContent
             title={t('journeys')}
             actions={
-                <Button onClick={() => setOpen('create')}>
-                    <PlusIcon />
-                    {t('create_journey')}
-                </Button>
+                <Dialog open={!!open} onOpenChange={(isOpen) => setOpen(isOpen ? 'create' : null)}>
+                    <DialogTrigger asChild>
+                        <Button size="lg">
+                            <PlusIcon />
+                            {t('create_journey')}
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>{t('create_journey')}</DialogTitle>
+                        </DialogHeader>
+                        <JourneyForm
+                            onSaved={async journey => {
+                                setOpen(null)
+                                await navigate(journey.id.toString())
+                            }}
+                        />
+                    </DialogContent>
+                </Dialog>
             }
         >
             <SearchTable
@@ -94,18 +116,6 @@ export default function Journeys() {
                 enableSearch
                 tagEntity="journeys"
             />
-            <Modal
-                onClose={() => setOpen(null)}
-                open={!!open}
-                title={t('create_journey')}
-            >
-                <JourneyForm
-                    onSaved={async journey => {
-                        setOpen(null)
-                        await navigate(journey.id.toString())
-                    }}
-                />
-            </Modal>
         </PageContent>
     )
 }
