7db07fc7a7b3af58449432582de0b4d78f44a5e3
RickjanHoornbeeck <51879@hoornbeeck.nl>
2026-03-23 11:31:37 +0100

    fix: possible checks fix

--- internal/pubsub/consumer/campaigns.go
+++ internal/pubsub/consumer/campaigns.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/lunogram/platform/internal/providers/channels"
@@ -110,13 +111,14 @@ func buildRenderData(publicURL string, user *subjects.User, campaign *management
 
 	data["now"] = time.Now()
 
-	data["preferences_url"] = fmt.Sprintf("%s/preferences/%s/%s", publicURL, campaign.ProjectID, user.ID)
+	baseURL := strings.TrimSuffix(publicURL, "/")
+	data["preferences_url"] = fmt.Sprintf("%s/preferences/%s/%s", baseURL, campaign.ProjectID, user.ID)
 
 	if !campaign.Transactional && campaign.SubscriptionID != nil {
 		unsubLink := url.Values{}
 		unsubLink.Set("u", user.ID.String())
 		unsubLink.Set("c", campaign.ID.String())
-		data["unsubscribe_url"] = fmt.Sprintf("%s/unsubscribe/email?link=%s", publicURL, url.QueryEscape("?"+unsubLink.Encode()))
+		data["unsubscribe_url"] = fmt.Sprintf("%s/unsubscribe/email?link=%s", baseURL, url.QueryEscape("?"+unsubLink.Encode()))
 	}
 
 	return data
